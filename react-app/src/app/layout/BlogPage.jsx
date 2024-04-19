e1491fa068b9456d396b741300ef627603958c74
Eljon02 <eljonkastrati@gmail.com>
2024-04-19 17:09:33 +0200

    US026 - First version of Blog Page (Frontend)

--- react-app/src/app/layout/BlogPage.jsx
+++ react-app/src/app/layout/BlogPage.jsx
@@ -15,7 +15,7 @@ export default function BlogPage() {
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const response = await axios.get('http://localhost:5052/api/articles');
+        const response = await axios.get('https://localhost:7153/api/Articles '); //5052
         setArticles(response.data);
       } catch (error) {
         console.error('Error fetching articles:', error);
@@ -26,19 +26,28 @@ export default function BlogPage() {
   }, []);
     
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">All Blogs</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {articles.map(article => {
-                  const newDate = formatDate(article.publicationDate)
-                  return (
-                      <div key={article.articleId} className="bg-white rounded-lg shadow-md p-6">
-                          <h2 className="text-xl font-semibold mb-2">{article.title}</h2>
-                          <p className="text-gray-600 mb-2">By {article.author} on {newDate}</p>
-                          <p className="text-gray-800">{article.content}</p>
-                      </div>
+    <div className="bg-slate-50 text-gray-700 p-6 min-h-screen items-center">
+      <div className="container mx-auto px-4 py-8">
+        <div className="container py-4 mb-8 border-b border-gray-300">
+          <h1 className="text-3xl font-bold mb-2 text-center">Blog Page</h1>
+          <h2 className="text-xl text-gray-600 mb-4 text-center">Read the most interesting articles from our best experst!</h2>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-10">
+                {articles.map(article => {
+                    const newDate = formatDate(article.publicationDate)
+                    return (
+                        <article key={article.articleId} className="bg-white shadow-[0.2em_0.2em_0.4em_#dfe1e2,-0.2em_-0.2em_0.4em_#fdfefe] hover:shadow-[0.6em_0.6em_0.8em_#dfe1e2,-0.6em_-0.6em_0.8em_#fdfefe] rounded-lg p-4 transition ease-in">
+                            <div className="mb-2"><img src="https://picsum.photos/600/600" alt="Description" className="rounded-lg object-cover max-h-60 w-full"></img></div>
+                            <p className="text-xs px-1 py-0.5 rounded-md mb-2 inline-block bg-slate-100 text-blue-500 hover:bg-blue-400 hover:text-white transition duration-300">{article.tags[0]}</p>
+                            <h2 className="text-xl font-semibold mb-2"><a href="/article/{article.id}">{article.title}</a></h2>
+                            <p className="text-gray-800 mb-2">{article.content}</p>
+                            <p className="text-gray-600">By {article.author} | {newDate}</p>
+                        </article>
+                      )
+                    }
                   )
-              })}
+                }
+        </div>
       </div>
     </div>
   )
--- react-app/src/app/layout/HomePage.jsx
+++ react-app/src/app/layout/HomePage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen flex flex-col justify-center items-center">
-        <h1 className="text-5xl font-extrabold text-white mb-8">Welcome to My Blog</h1>
-        <p className="text-lg text-white mb-12">Explore insightful articles on various topics.</p>
-        <a href="/blog" className="bg-white text-blue-500 hover:bg-blue-400 hover:text-white font-semibold py-3 px-8 rounded-full transition duration-300">
-            Read the Blog
-        </a>
+    <div className="bg-slate-50 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-extrabold mb-2">Welcome to My Blog</h1>
+      <p className="text-lg text-gray-600 mb-12">Explore insightful articles on various topics.</p>
+      <a href="/blog" className="bg-white border border-blue-500 text-blue-500 hover:bg-blue-400 hover:text-white font-semibold py-3 px-8 rounded-md transition duration-300">
+          Read the Blog
+      </a>
     </div>
   )
 }
