d32305e2ae99fe769dbb6aa45efa69e94d7cce35
Hajrije3 <120335805+Hajrije3@users.noreply.github.com>
2024-04-20 18:28:47 +0200

    US026 - Create add/edit form for articles

--- react-app/src/app/layout/HomePage.jsx
+++ react-app/src/app/layout/HomePage.jsx
@@ -9,6 +9,10 @@ export default function HomePage() {
         <a href="/blog" className="bg-white text-blue-500 hover:bg-blue-400 hover:text-white font-semibold py-3 px-8 rounded-full transition duration-300">
             Read the Blog
         </a>
+
+        <Link to="/ArticleForm" className="bg-white text-blue-500 hover:bg-blue-400 hover:text-white font-semibold py-3 px-8 rounded-full transition duration-300">
+            Add/Edit
+        </Link>
     </div>
   )
 }
