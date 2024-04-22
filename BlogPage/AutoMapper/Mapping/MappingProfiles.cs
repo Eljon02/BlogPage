5fedd33911f91ac0597b8c7d856adef64e19d82e
Genti Krasniqi <59203667+gentikk@users.noreply.github.com>
2024-04-22 23:59:13 +0200

    Update MappingProfiles.cs

--- BlogPage/AutoMapper/Mapping/MappingProfiles.cs
+++ BlogPage/AutoMapper/Mapping/MappingProfiles.cs
@@ -1,2 +1,21 @@
 ﻿using AutoMapper;
+using BlogPage.Models.DTOs;
+using BlogPage.Models;
+using System.Reflection.Metadata;
+
+namespace BlogPage.AutoMapper.Mapping
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Comment, Profiles.CommentProfile>();
+
+            CreateMap<Article, ArticleDto>()
+                .ForMember(d => d.Comments, o => o.MapFrom(a => a.Comments));
+
+            CreateMap<Comment, CommentDto>();
+        }
+    }
+}
 
