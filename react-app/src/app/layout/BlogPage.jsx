ca22a3d23573ae5a5927e71f910c3f734bda02bb
KaltrinaKrasniqi <59203667+gentikk@users.noreply.github.com>
2024-04-23 02:05:17 +0200

    Test

--- react-app/src/app/layout/BlogPage.jsx
+++ react-app/src/app/layout/BlogPage.jsx
@@ -38,7 +38,7 @@ export default function BlogPage() {
       <div className="bg-slate-50 text-gray-700 p-6 min-h-screen items-center">
         <div className="container mx-auto px-4 py-8">
           <div className="container py-4 mb-8 border-b border-gray-300">
-            <h1 className="text-3xl font-bold mb-2 text-center">Blog  Page</h1>
+            <h1 className="text-3xl font-bold mb-2 text-center">Blog Page</h1>
             <h2 className="text-xl text-gray-600 mb-4 text-center">Read the most interesting articles, written from our best experst!</h2>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-10">
