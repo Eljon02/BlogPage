85b1b5341b87cd89953a6b8fbf53f1d46de71f8e
gentikk <59203667+gentikk@users.noreply.github.com>
2024-04-29 12:18:06 +0200

    US026 - Design the page to show article's details.

--- react-app/src/app/layout/ArticlePage.jsx
+++ react-app/src/app/layout/ArticlePage.jsx
@@ -6,6 +6,7 @@ import CommentForm from './CommentForm';
 import { Link } from 'react-router-dom';
 import {v4 as uuid} from 'uuid'
 
+// Function to format date string
 const formatDate = (dateString) => {
     const date = new Date(dateString);
     const year = date.getFullYear();
@@ -14,12 +15,14 @@ const formatDate = (dateString) => {
     return `${year}-${month}-${day}`;
 };
 
+// Functional component for displaying article and its comments
 const ArticlePage = () => {
   const { id } = useParams();
   const [article, setArticle] = useState([]);
   const [newestArticles, setNewestArticles] = useState([]);
   const [comments, setComments] = useState([]);
 
+  // useEffect hook to fetch article and comments data
   useEffect(() => {
     const fetchArticle = async (id) => {
       try {
@@ -55,6 +58,7 @@ const ArticlePage = () => {
     fetchNewestArticles();
   }, [id]);
 
+  // Function to handle comment submission
   const handleSubmitComment = async (commentData) => {
     try {
       const response = await axios.post(`https://localhost:7153/Api/Comment/${id}`, {
@@ -72,12 +76,16 @@ const ArticlePage = () => {
         <Navbar />
         <div className="text-gray-700 p-6 min-h-screen">
             <div className="container mx-auto px-4">
-            <h2 className="text-3xl font-semibold text-center">{article.title}</h2>
-                <div className="mb-2 flex justify-center">
-                    <img src="https://picsum.photos/600/400" alt="Description" className="rounded-lg object-cover w-auto h-auto" />
-                </div>
-                <p className="text-gray-600 mb-4 text-center">By {article.author} | Published on {formatDate(article.publicationDate)}</p>
-                <p className="text-gray-800 text-center">{article.content}</p>
+              <h2 className="text-3xl mb-2 font-semibold text-center">{article.title}</h2>
+              <div className="mb-2 flex justify-center">
+                  {article.photo ? (
+                    <img src={article.photo.url} alt="Description" className="rounded-lg object-cover max-h-400 w-full"></img>
+                  ) : (
+                  <img src="https://picsum.photos/600/400" alt="Description" className="rounded-lg object-cover w-auto h-auto" />
+                  )}
+              </div>
+              <p className="text-gray-600 mb-4 text-center">By {article.author} | Published on {formatDate(article.publicationDate)}</p>
+              <p className="text-gray-800 text-left">{article.content}</p>
             </div>
         </div>
         <div className="container max-w-4xl mx-auto px-4 py-8 mb-4 border-t border-gray-300">
@@ -97,9 +105,9 @@ const ArticlePage = () => {
                 {newestArticles.map((newestArticle, index) => (
                     <div key={index} className="bg-white shadow-[0.2em_0.2em_0.4em_#dfe1e2,-0.2em_-0.2em_0.4em_#fdfefe] hover:shadow-[0.6em_0.6em_0.8em_#dfe1e2,-0.6em_-0.6em_0.8em_#fdfefe] rounded-lg p-4 transition ease-in">
                         <h3 className="text-lg font-semibold mb-2 text-center hover:text-blue-600">
-                            <Link 
-                                to={`/article/${newestArticle.articleId}`} 
-                                onClick={() => window.scrollTo(0, 0)} // Scroll to top when link is clicked
+                            <Link
+                                to={`/article/${newestArticle.articleId}`}
+                                onClick={() => window.scrollTo(0, 0)}
                             >
                                 {newestArticle.title}
                             </Link>
